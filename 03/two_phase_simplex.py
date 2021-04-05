f030211b80feaa20284e0974e0704a37212136ff
aleksakojadinovic <kojadinovic.aleksa98@gmail.com>
2021-04-05 11:45:44 +0200

    started transportation problem

--- 03/two_phase_simplex.py
+++ 03/two_phase_simplex.py
@@ -1,7 +1,7 @@
 import numpy as np
 import lp_input_parser as lpp
 import dual_simplex as ds
-
+import simplex as revised_simplex
 tfLOG = True
 DEC = 8
 
@@ -20,31 +20,65 @@ def get_modified_problem(simplex_matrix):
     m, n = simplex_matrix.shape
     A = simplex_matrix[:-1, :-1]
     b = simplex_matrix[:, -1]
+    c = simplex_matrix[-1, :]
     modified_simplex_matrix = np.zeros((m, n + m - 1), dtype=simplex_matrix.dtype)
     modified_simplex_matrix[:A.shape[0], :A.shape[1]] = A
     modified_simplex_matrix[:, -1] = b
     modified_simplex_matrix[:-1, A.shape[1]:-1] = np.eye(m - 1)
-    modified_simplex_matrix[-1, n:] = np.ones(m-1)    
+    print(modified_simplex_matrix)
+
+    modified_simplex_matrix[-1, n-1:] = c[:-1]
     return modified_simplex_matrix
 
-def two_phase_simplex(simplex_matrix):
 
+def two_phase_simplex(simplex_matrix):
+    simplex_matrix = np.array(simplex_matrix, dtype='float64')
+    m, n = simplex_matrix.shape
+    num_vars = n - 1
     ### PHASE 1
+    tflog('--PHASE 1')
     tflog(f'Original:\r\n {simplex_matrix}')
     modified_simplex_matrix = get_modified_problem(simplex_matrix)    
     tflog(f'Modified:\r\n {modified_simplex_matrix}')
 
-    omega, omega_val, omega_last_matrix = ds.dual_simplex(modified_simplex_matrix, True)
-    tflog(f'omega solution: {omega_val}')
-    tflog(f'omega last matrix:')
-    tflog(omega_last_matrix)
+    _, omega_val, omega_last_matrix = ds.dual_simplex(modified_simplex_matrix, True)
+    print(omega_last_matrix)
+    # if omega_val != 0:
+    #     tflog(f'Omega val is {omega_val}, stopping.')
+    #     return None, None
+
+    # ### PHASE 2
+    # tflog('--PHASE 2')
+    # tflog('Last known omega matrix:')
+    # tflog(omega_last_matrix)
+
+    # # Now find indices of all basis columns
+    # omega_m, omega_n = omega_last_matrix.shape
+    # basis_indices = []
+    # for col_index in range(omega_n - 1):
+    #     candidate_column = omega_last_matrix[:-1, col_index]
+    #     if len(candidate_column[candidate_column == 1]) == 1 and len(candidate_column[candidate_column == 0]) == len(candidate_column) - 1:
+    #         tflog(f'\t{candidate_column} is basic.')
+    #         basis_indices.append(col_index)
+
+    # if len(basis_indices) != m - 1:
+    #     raise ValueError(f'**************** COMES RIGHT BACK')
+
+    # tflog(f'Columns with indices: {basis_indices} form a max-rank basis submatrix.')
+            
+        
+    
+
+
+
 
     return None, None
 
-arg_parser = lpp.get_simplex_parser()
-args = vars(arg_parser.parse_args())
-tfLOG = args['logging']
-smatrix, _ = lpp.prepare_for_algorithm(args)
+if __name__ == '__main__':
+    arg_parser = lpp.get_simplex_parser()
+    args = vars(arg_parser.parse_args())
+    tfLOG = args['logging']
+    smatrix, _ = lpp.prepare_for_algorithm(args)
 
-x, v = two_phase_simplex(smatrix)
-# lpp.print_solution(args, x, v)
+    x, v = two_phase_simplex(smatrix)
+    lpp.print_solution(args, x, v)
---
+++ 04/transport.py
@@ -0,0 +1,111 @@
+import numpy as np
+
+### The transportation problem
+
+# There are `m` shops, indexed with `i`
+# and `n` warehouses indexed with `j`
+# `c_ij` is the cost of transportation from `i` to `j`
+# `x_ij` is the transported amount (target variables)
+# `a_i` is the amount of goods at warehouse `i` (SUPPLY)
+# `b_j` is the required amount of goods are shop `j` (DEMAND)
+
+# Find optimal x_ij.
+
+### MODEL:
+# (min) sum sum c_ij*x_ij
+# such that
+# sum_{i=0,m-1} x_ij <= a_i  for all i  (DO NOT EXCEED SUPPLY)
+# sum_{i=0,n-1} x_ij <= b_j  for all j  (DO NOT EXCEED DEMAND)
+# xij >= 0                              (NON NEGATIVE AMOUNTS)
+
+# TODO: Check if this can be refactored
+def argmin_exclude(A, ex_rows, ex_columns):
+    min_i = None
+    min_j = None
+    min_val = None
+
+    for i, row in enumerate(A):
+        for j, element in enumerate(row):
+            if i in ex_rows or j in ex_columns:
+                continue
+            if min_val is None or element < min_val:
+                min_i = i
+                min_j = j
+                min_val = element
+
+    return min_i, min_j
+
+
+# Method of minimal prices
+def min_prices_method(C, a, b):
+    C = np.array(C, dtype='float64')
+    a = np.array(a, dtype='float64')
+    b = np.array(b, dtype='float32')
+    X = np.zeros(C.shape)
+    m, n = C.shape
+
+    X = np.zeros(C.shape)
+    cap_mask = np.zeros(C.shape)
+    removed_rows = np.array([], dtype='int')
+    removed_columns = np.array([], dtype='int')
+    
+    for iteration in range(m + n - 1):
+        p, q = argmin_exclude(C, removed_rows, removed_columns)
+        the_min = min(a[p], b[q])
+        X[p][q] = the_min
+
+        a[p] = a[p] - the_min
+        b[q] = b[q] - the_min
+
+        if a[p] == 0:
+            removed_rows = np.append(removed_rows, p)
+        if b[q] == 0:
+            removed_columns = np.append(removed_columns, q)
+
+        cap_mask[p][q] = 1
+
+    
+
+
+    return X, cap_mask
+
+def potential_method(C, a, b, basis_solution, caps):
+    print(f'>> Potential method')
+    
+
+
+
+
+    
+        
+def problem_matrix_to_cab(problem_matrix):
+    problem_matrix = np.array(problem_matrix)
+    C = problem_matrix[:-1, :-1]
+    a = problem_matrix[:-1, -1]
+    b = problem_matrix[-1, :-1]
+    return C, a, b
+
+
+
+def example1():
+    mat      = [[20, 11, 15, 13, 2],
+                [17, 14, 12, 13, 6],
+                [15, 12, 18, 18, 7],
+                [3, 3, 4, 5, 0]]
+
+    C, a, b = problem_matrix_to_cab(mat)
+
+    
+    X, cap_mask = min_prices_method(C, a, b)
+    potential_method(C, a, b, X, cap_mask)
+
+if __name__ == '__main__':
+    example1()
+
+
+
+        
+    
+    
+    
+
