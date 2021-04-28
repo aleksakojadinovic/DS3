e4df307992f99cbf20ae2301a6452c6255dd2312
aleksakojadinovic <kojadinovic.aleksa98@gmail.com>
2021-04-28 18:35:57 +0200

    Start two phase simplex

--- 03_dual_and_two_phase_simplex/two_phase_simplex.py
+++ 03_dual_and_two_phase_simplex/two_phase_simplex.py
@@ -1,5 +1,5 @@
 import numpy as np
-from simplex import canonical_simplex as revised_simplex
+from regular_simplex import reg_simplex as r_simplex
 
 FLOAT_T = 'float32'
 
@@ -42,6 +42,7 @@ def prep_for_phase_one(eqA):
     return eqA, artificial_indices, all_basis_indices
     
 
+
 # We add m artifical variables and we do not care if some other were basic
 # I have no idea whether this will work
 def simple_prep(eqA):
@@ -69,6 +70,29 @@ def get_sub_simplex_matrix(subA, eqb, artificial_indices):
         sub_simplex_matrix[-1, :] -= sub_simplex_matrix[i, :]
 
     return sub_simplex_matrix, artificial_indices, [i for i in range(n) if i not in artificial_indices]
+
+def piv_artif(sub_simplex_matrix, artificial_indices):
+    for i, artifical_idx in enumerate(artificial_indices):
+        sub_simplex_matrix[-1, :] -= sub_simplex_matrix[i, :]
+
+    return sub_simplex_matrix
+
+def fix_indices(index_collection, removed_index):
+    for i in range(len(index_collection)):
+        if index_collection[i] >= removed_index:
+            index_collection[i] -= 1
+
+    return index_collection
+
+def find_basic_columns(mat):
+    bs = []
+    for i in range(mat.shape[1]):
+        col = mat[:, i]
+        if len(col[col == 1]) == 1 and len(col[col == 0]) == len(col) - 1:
+            bs.append(i)
+
+    return bs
+
 
 # Assumes all constrains have been converted to equalities
 def two_phase_simplex_solver(c, eqA, eqb):
@@ -102,12 +126,111 @@ def two_phase_simplex_solver(c, eqA, eqb):
     x0length = sub_n - 1
     x0 = np.append(np.zeros(x0length - len(sub_b_vector)), sub_b_vector)
 
-    vector, value, final_basic_indices, final_nonbasic_indices, niter = revised_simplex(subA, np.array(non_basic_indices), np.array(basic_indices), x0)
-    print(f'Vector: {vector}')
-    print(f'Value: {value}')
-    print(f'Basic: {final_basic_indices}')
-    print(f'Nonbasic: {final_nonbasic_indices}')
-    print(f'Niter: {niter}')
+    indicator, last_matrix, last_basic_indices = r_simplex(subA, basic_indices)
+
+    print(f'The algorithm was {"successful" if indicator else "UNSUCCESSFUL"}')
+    print(f'last simplex matrix: ')
+    print(last_matrix)
+    print(f'Last list of basic:')
+    print(last_basic_indices)
+
+    phase_one_opt = last_matrix[-1, -1]
+    print(f'Phase 1 opt: {phase_one_opt}')
+
+    if not np.isclose(phase_one_opt, 0.0):
+        print(f'Phase one opt is nonzero, no solution then.')
+        print(f'it is: {phase_one_opt}')
+        return None
+
+
+
+    # first we remove all artificial variables that are not basic
+    print(f'artificial: {artificial_indices}')
+    cols_to_delete = []
+    for artif_index in artificial_indices:
+        if artif_index in last_basic_indices:
+            continue
+        cols_to_delete.append(artif_index)
+        print(f'Variable {artif_index} is artif. and nonbasic, so we remove that column')
+
+    artificial_indices = [a for a in artificial_indices if a not in cols_to_delete]
+    last_basic_indices = [a for a in last_basic_indices if a not in cols_to_delete]
+
+    new_matrix = last_matrix.copy()
+    for j, col_to_delete in enumerate(cols_to_delete):
+        for i in range(len(artificial_indices)):
+            if artificial_indices[i] >= col_to_delete:
+                artificial_indices[i] -= 1
+        for i in range(len(last_basic_indices)):
+            if last_basic_indices[i] >= col_to_delete:
+                last_basic_indices[i] -= 1
+
+        for k in range(j+1, len(cols_to_delete)):
+            if cols_to_delete[k] >= col_to_delete:
+                cols_to_delete[k] -= 1
+
+        new_matrix = np.delete(new_matrix, [col_to_delete], axis=1)
+
+    
+    print(f'After removing artificial that are non-basic:')
+    print(new_matrix)
+    print(f'artificial: {artificial_indices}')
+    print(f'basic: {last_basic_indices}')
+
+    # Now all artificial are also basic
+
+    cols_to_delete = []
+    for art_and_basic in artificial_indices:
+        column = new_matrix[:, art_and_basic]
+        row_idx = np.argwhere(new_matrix[:, art_and_basic] == 1)[0][0]
+        row = new_matrix[row_idx]
+        
+        if (len(row[row == 0]) == len(row) - 1):
+
+            # TODO: remove this row and this column
+            artificial_indices.remove(art_and_basic)
+            last_basic_indices.remove(art_and_basic)
+            cols_to_delete.append(art_and_basic)
+            new_matrix = np.delete(new_matrix, [row_idx], axis=0)
+
+
+    new_matrix = new_matrix.copy()
+    for j, col_to_delete in enumerate(cols_to_delete):
+        for i in range(len(artificial_indices)):
+            if artificial_indices[i] >= col_to_delete:
+                artificial_indices[i] -= 1
+        for i in range(len(last_basic_indices)):
+            if last_basic_indices[i] >= col_to_delete:
+                last_basic_indices[i] -= 1
+
+        for k in range(j+1, len(cols_to_delete)):
+            if cols_to_delete[k] >= col_to_delete:
+                cols_to_delete[k] -= 1
+
+        new_matrix = np.delete(new_matrix, [col_to_delete], axis=1)   
+            
+    print(artificial_indices)
+    # The rest should be pivoted
+    # TODO: PIVOT REST
+
+    new_matrix_n = new_matrix.shape[1]
+    if new_matrix_n == len(c):
+        new_matrix[-1, :] = np.vstack((new_matrix, c))
+    else:
+        diff = new_matrix.shape[1] - len(c)
+        new_target = np.append(c, np.zeros(diff))
+        new_matrix[-1, :] = new_target
+
+    phase_two_matrix = piv_artif(new_matrix, find_basic_columns(new_matrix[:-1, :-1]))
+
+    print(phase_two_matrix)
+
+    ind, mat, _ = r_simplex(phase_two_matrix, [])
+
+    if ind:
+        print(mat)
+        print(f'OPTIMAL: {np.round(mat[-1, -1], 8)}')
+    
     
     
 def example1():
@@ -132,5 +255,25 @@ def example2():
 
     two_phase_simplex_solver(c, A, b)
 
+def example3():
+    c = [3, 1, 4, 0, 0, 0, 0]
+    A = [[1, 3, 1, 1, 0, 0, 0],
+         [-3, -1, 1, 0, 1, 0, 0],
+         [3, 1, 3, 0, 0, 1, 0],
+         [1, 0, 0, 0, 0, 0, 1]]
+
+    b = [10, -2, 6, 1]
+
+    two_phase_simplex_solver(c, A, b)
+
+def example4():
+    c = [7, 4, 1, 0, 0, 0]
+    A = [[-2, 1, 1, 1, 0, 0],
+         [-1, -2, -1, 0, 1, 0],
+         [1, -1, 2, 0, 0, 1]]
+
+    b = [0, -3, 4]
+    two_phase_simplex_solver(c, A, b)
+
 if __name__ == '__main__':
-    example1()
+    example4()
